5937f9cdc66065dd20818d0e8df4a7dd35713fcb
IbragimAli1086 <Ibragimali2999@gmail.com>
2025-06-19 15:33:47 +0000

    Add _putchar function and main program that prints _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-int _putchar(char c);
+#include "main.h"
 
+/**
+ * main - _putchar sözünü çap edir, sonra yeni sətrə keçir
+ *
+ * Return: 0 (uğurla bitir)
+ */
 int main(void)
 {
-    char *str = "_putchar";
-    int i = 0;
-
-    while (str[i])
-    {
-        _putchar(str[i]);
-        i++;
-    }
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
     _putchar('\n');
     return (0);
 }
+
