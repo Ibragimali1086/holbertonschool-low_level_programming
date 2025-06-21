9a0a759597c0768d4d398d65579e338f97571bc7
IbragimAli1086 <Ibragimali2999@gmail.com>
2025-06-21 11:50:10 +0000

    Add keygen program generating random passwords for 101-crackme

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -3,23 +3,28 @@
 #include <time.h>
 
 /**
- * main - Generates a random password for 101-crackme
- * Return: 0
+ * main - Generates the password for 101-crackme
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-    int i;
-    char password[7]; // Parol 6 simvoldan ibarətdir, sonuncu '\0' üçün yer
+    char password[7];
+    int i, sum = 0;
 
-    srand(time(NULL)); // Random toxumu təyin et
+    srand(time(NULL));
 
     for (i = 0; i < 6; i++)
     {
-        password[i] = 'a' + rand() % 26; // a-z arasında təsadüfi simvol
+        password[i] = 33 + rand() % 94; // ASCII 33-126 aralığında təsadüfi simvol
+        sum += password[i];
     }
-    password[6] = '\0'; // string bitirici
+    password[6] = '\0';
+
+    // Əlavə məntiq - məsələn, sum müəyyən şərti ödəməlidir (bu nümunə deyil)
+    if ((sum % 10) != 0)
+        password[0] = 'H';  // nümunə üçün şərt
 
     printf("%s\n", password);
-
     return (0);
 }
