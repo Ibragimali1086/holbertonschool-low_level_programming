776d4d6886376920a83101c71983ad01c189952f
IbragimAli1086 <Ibragimali2999@gmail.com>
2025-06-21 18:39:12 +0000

    Implement infinite_add function to add two numbers as strings

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,37 +1,63 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers stored as strings
- * @n1: first number
- * @n2: second number
- * @r: buffer to store result
- * @size_r: size of buffer
- * Return: pointer to result, or 0 if result can't be stored in r
+ * infinite_add - adds two positive numbers stored as strings
+ * @n1: first number string
+ * @n2: second number string
+ * @r: buffer to store the result
+ * @size_r: size of the buffer r
+ *
+ * Return: pointer to result (r), or 0 if result can't be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i = 0, j = 0, k = 0, l = 0, m = 0, carry = 0, digit1 = 0, digit2 = 0, sum = 0;
+	int i = 0, j = 0, k = 0, sum, carry = 0;
+	int digit1, digit2;
 
-    while (n1[i] != '\0')
-        i++;
-    while (n2[j] != '\0')
-        j++;
-    i--;
-    j--;
+	/* get length of n1 */
+	while (n1[i] != '\0')
+		i++;
 
-    if (size_r <= (i > j ? i : j) + 2)
-        return (0);
+	/* get length of n2 */
+	while (n2[j] != '\0')
+		j++;
 
-    while (i >= 0 || j >= 0 || carry)
-    {
-        digit1 = (i >= 0) ? n1[i--] - '0' : 0;
-        digit2 = (j >= 0) ? n2[j--] - '0' : 0;
-        sum = digit1 + digit2 + carry;
-        carry = sum / 10;
-        r[k++] = (sum % 10) + '0';
-    }
+	i--; /* last index of n1 */
+	j--; /* last index of n2 */
+	k = 0; /* index for result buffer */
 
-    if (k >= size_r)
-        return (0);
+	/* Check if buffer is large enough for result */
+	/* max length result can be max(i, j) + 2 (for carry and '\0') */
+	if (size_r <= (i > j ? i : j) + 2)
+		return (0);
 
-    r[k] = '
+	/* Add digits from right to left */
+	while (i >= 0 || j >= 0 || carry)
+	{
+		digit1 = (i >= 0) ? n1[i] - '0' : 0;
+		digit2 = (j >= 0) ? n2[j] - '0' : 0;
+
+		sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+
+		if (k >= size_r - 1) /* no space for next digit + null terminator */
+			return (0);
+
+		r[k] = (sum % 10) + '0';
+		k++;
+		i--;
+		j--;
+	}
+
+	r[k] = '\0';
+
+	/* Reverse the result string */
+	for (i = 0, j = k - 1; i < j; i++, j--)
+	{
+		char tmp = r[i];
+		r[i] = r[j];
+		r[j] = tmp;
+	}
+
+	return (r);
+}
