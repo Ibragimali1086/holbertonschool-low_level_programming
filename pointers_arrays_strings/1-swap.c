bf3f8bb1c85066d2a1930bbc12934b331372eb13
IbragimAli1086 <Ibragimali2999@gmail.com>
2025-06-21 10:03:38 +0000

    Implement swap_int function to swap two integers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,18 @@
+/**
+ * File: 1-swap.c
+ * Author: Your Name
+ * Description: Function that swaps the values of two integers.
+ */
+
 #include "main.h"
 
 /**
  * swap_int - swaps the values of two integers
  * @a: pointer to first integer
  * @b: pointer to second integer
+ *
+ * Description: This function swaps the values stored at the
+ * memory locations pointed to by @a and @b.
  */
 void swap_int(int *a, int *b)
 {
