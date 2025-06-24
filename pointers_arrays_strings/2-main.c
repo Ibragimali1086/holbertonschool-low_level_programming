296291438fe596898fb3feff768359a504377f98
IbragimAli1086 <Ibragimali2999@gmail.com>
2025-06-24 14:04:18 +0000

    Implement _strchr function according to Betty style

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -8,14 +8,22 @@
  */
 int main(void)
 {
-    char *s = "hello";
-    char *f;
+	char *s = "First, solve the problem. Then, write the code.";
+	char *f;
 
-    f = _strchr(s, 'l');
+	f = _strchr(s, ',');
+	if (f != NULL)
+		printf("Found ',' at: %s\n", f);
 
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
+	f = _strchr(s, 'a');
+	if (f != NULL)
+		printf("Found 'a' at: %s\n", f);
+	else
+		printf("Character 'a' not found\n");
+
+	f = _strchr(s, '\0');
+	if (f != NULL)
+		printf("Found '\\0' at: %s\n", f);
+
+	return (0);
 }
--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,27 @@
 #include "main.h"
+#include <stddef.h> /* for NULL */
 
 /**
  * _strchr - locates a character in a string
- * @s: string to search in
- * @c: character to locate
- * 
- * Return: pointer to first occurrence of c in s, or NULL if not found
+ * @s: string to search
+ * @c: character to find
+ *
+ * Return: pointer to first occurrence of c in s,
+ *         or NULL if c is not found
  */
 char *_strchr(char *s, char c)
 {
-    int i;
+	int i = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] == c)
-            return (&s[i]);
-    }
-    if (c == '\0') /* if searching for '\0', return pointer to end of string */
-        return (&s[i]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
 
-    return (NULL);
+	if (c == '\0')
+		return (s + i);
+
+	return (NULL);
 }
