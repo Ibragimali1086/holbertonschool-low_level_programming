79c950740be603110fc8fc42c0e3d1c6f6339555
IbragimALi1086 <Ibragimali2999@gmail.com>
2025-07-23 20:03:42 +0000

    Implement print_list function and test it in main

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,25 +2,25 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the head of the list
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to the head of the list
  *
- * Return: number of nodes
+ * Return: Number of nodes
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 
-        h = h->next;
-        count++;
-    }
+		h = h->next;
+		count++;
+	}
 
-    return (count);
+	return (count);
 }
